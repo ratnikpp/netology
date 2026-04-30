03940ef5353538f4d1626a472b2e92e3ea511eb8
Ratnikov Pavel <39724877+ratnikpp@users.noreply.github.com>
2026-04-30 10:20:51 +0300

    Update logical_type_2.js

--- logical_type_2.js
+++ logical_type_2.js
@@ -1,1 +1,8 @@
+let user = 'netologist';
+let orderOwner = 'leo_tolstoy';  
 
+if (user === orderOwner || user === 'admin') {
+    console.log('Редактирование разрешено');
+} else {
+    console.log('Заказ нельзя редактировать');
+}
