8b7cba754dbec5f38e6e948c821a3f37eedd0ef1
Ratnikov Pavel <39724877+ratnikpp@users.noreply.github.com>
2026-04-30 10:16:38 +0300

    Update logical_type_1.js

--- logical_type_1.js
+++ logical_type_1.js
@@ -1,1 +1,16 @@
+let userName = 'Vova';
+let userBalance = 90;
 
+if (userName === 'admin') {
+    console.log('Администратор');
+} else if (userName === '') {
+    console.log('Гость');
+} else {
+    if (userBalance >= 5000) {
+        console.log('Вип-клиент');
+    } else if (userBalance > 1000) {
+        console.log('Постоянный покупатель');
+    } else {
+        console.log('Пользователь');
+    }
+}
