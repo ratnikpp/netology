341c78057e18ba33ad41c6749de479592db4e69b
Ratnikov Pavel <39724877+ratnikpp@users.noreply.github.com>
2026-04-30 10:23:03 +0300

    Update logical_type_3.js

--- logical_type_3.js
+++ logical_type_3.js
@@ -1,1 +1,15 @@
+let bonusBalance = 500; 
+let purchasesThisMonth = 2;
+let purchaseAmount = 3000; 
 
+const baseRate = 10;
+const premiumRate = 20;
+const frequentBonus = 5;
+
+let finalRate = bonusBalance > 5000 ? premiumRate : baseRate;
+
+if (purchasesThisMonth > 1) {
+    finalRate += frequentBonus;
+}
+
+console.log(`Процент от покупки: ${finalRate}%`);
